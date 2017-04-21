11f55fdfee808ef4ec536c0c4622e5467754bd56
Regner Blok-Andersen <shadowdf@gmail.com>
2017-04-21 14:05:26 -0700

    Put outputs into version folder and name files with version name

--- aodb/main.py
+++ aodb/main.py
@@ -1,5 +1,8 @@
 
 
+from os import mkdir
+from os.path import exists
+
 from . import exporters
 from.resources import RESOURCES
 
@@ -12,23 +15,28 @@ def get_input_path(name: str, input_folder: str = DEFAULT_INPUT) -> str:
     return f'{input_folder}{name}.txt'
 
 
-def get_output_path(name: str, output_folder: str = DEFAULT_OUTPUT) -> str:
-    return f'{output_folder}{name}.' + '{}'
+def get_output_version_folder(version: str, output_folder: str = DEFAULT_OUTPUT) -> str:
+    return f'{output_folder}{version}/'
+
+
+def get_output_path(name: str, version: str, output_folder) -> str:
+    return f'{output_folder}{version}-{name}.' + '{}'
 
 
 def generate_exports(version: str):
     """Generates the database conversions."""
-    # Load the raw data
-    # Run export
-
     print('Generating exports...')
+
+    output_folder = get_output_version_folder(version)
+    if not exists(output_folder):
+        mkdir(output_folder)
 
     for resource in RESOURCES:
         name = resource['name']
         print(f'Exporting {name}...')
 
         input_path = get_input_path(name)
-        output_path = get_output_path(name)
+        output_path = get_output_path(name, version, output_folder)
 
         exporters.XmlExporter(input_path, output_path).genreate_export()
         exporters.JsonExporter(input_path, output_path).genreate_export()
